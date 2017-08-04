3853f708ec9be59f4651115fba836f538962b1dd
cylau1031 <cylau1031@gmail.com>
2017-08-04 18:10:39 -0400

    finishing installation

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const body-parser = require('body-parser');
+const bodyParser = require('body-parser');
 const route = require('./routes');
-const nunjucks = require('nunjucks')
-express.static('./public');
+const nunjucks = require('nunjucks');
+const app = express();
+const path = require('path');
+
 
 var env = nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
+
+app.use(bodyParser.urlencoded({extende: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', );
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
 const pg = require('pg');
+const pgHstore = require('pghstore');
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistackdb');
